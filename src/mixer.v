bc766ed80fec24fcf1353e611158af50a4f097fd
Colby Miller <54876247+Colbywonn@users.noreply.github.com>
2026-08-19 17:17:58 -0600

    Update mixer.v with comments
    Updated with comments

--- src/mixer.v
+++ src/mixer.v
@@ -1,8 +1,16 @@
+/*
+Date: 08-19-2026
+Author: Colby Miller
+TinyTapeout Sky26c Frozen Version
+
+This module is very simple. It adds together the outputs of the three voices, and outputs it.
+*/
+
 module mixer (
     input  signed [13:0] in_0,
     in_1,
     in_2,
-    output signed [15:0] out
+    output signed [15:0] out //16-bits ensures no overflow is possible, three signed inputs need 2 bits of overhead total.
 );
   assign out = in_0 + in_1 + in_2;
 
